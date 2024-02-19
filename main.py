989e6fc9546c671cff49ad769abf2b36a84d6895
sanket363@gmail.com <sanketbhalke363@gmail.com>
2024-02-19 21:10:56 +0530

    commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? \n$")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be a greater than zero")
+        else:
+            print("Please Enter a number")
+    
+    return amount
+
+deposit()
